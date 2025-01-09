6723030f49f5777a7d79ace8c692505a42dd5543
Denis Matteo Bottan <148993354+DenixBotTv@users.noreply.github.com>
2025-01-09 20:57:49 +0100

    Update bst_lib.cpp

--- bst_lib.cpp
+++ bst_lib.cpp
@@ -1,234 +1,201 @@
-
-
-
-
-void Node:: inOrder(){
-    if(this->lchild!=nullptr){
-        this->lchild->preOrder();
-    }
-    cout<<this->data<<" ";
-    if(this->rchild!=nullptr){
-        this->rchild->preOrder();
-    }
-}
-
-
-void Node:: preOrder(){
-    cout<<this->data<<" ";
-    if(this->lchild!=nullptr){
-        this->lchild->preOrder();
-    }
-    if(this->rchild!=nullptr){
-        this->rchild->preOrder();
-    }
-}
-
-void Node:: postOrder(){
-    if(this->lchild!=nullptr){
-        this->lchild->postOrder();
-    }
-    if(this->rchild!=nullptr){
-        this->rchild->postOrder();
-    }
-    cout<<this->data<<" ";
-}
-
-
-
-
-
-
-
-
-Node* Node::deleteNode(int k) {
-    if (this->data == nullptr) {
-        cout << "Non posso cancellare un nodo che non c'è" << endl;
-        return this;
-    }
-    if (k == this->data) {
-        if (this->lchild == nullptr && this->rchild == nullptr) {
-            delete this;
-            return nullptr;
-        } else if (this->lchild == nullptr && this->rchild != nullptr) {
-            Node* temp;
-            temp = this->rchild;
-            this->data = temp->data;
-            this->rchild = temp->rchild;
-            delete temp;
-            return this;
-        } else if (this->lchild != nullptr && this->rchild == nullptr) {
-            Node* temp;
-            temp = this->lchild;
-            this->data = temp->data;
-            this->lchild = temp->lchild;
-            delete temp;
-            return this;
-        }
-    }
-    if (k < this->data) {
-        if (this->lchild != nullptr)
-            this->lchild = this->lchild->deleteNode(k);
-    } else {
-        if (this->rchild != nullptr)
-            this->rchild = this->rchild->deleteNode(k);
-    }
-    return this;
-}
-
-
-
-
-bool Node::searchR(int k) {                     // ricerca ricorsiva
-    bool check;
-    if (this == NULL) {
-        check = false;
-        return check;
-    }
-    if (this->data == k) {
-        check = true;
-        return check;
-    }
-    if (k < this->data) {
-        check = this->lchild->searchR(k);
-    }
-    else if (k > this->data) {
-        check = this->rchild->searchR(k);
-    }
-    return check;
-}
-
-
-
-
-bool Node:: searchI(int k){ //ricerca iterativa
-    bool check{true};
-    Node *current{this};
-    while (check){
-
-        if (current==NULL){
-            cout << "non trovato" << " ";
-            return false;
-        }
-
-        if(current->data==k){
-            cout << "trovato" << " ";
-            return true;
-        }
-        if (k<current->data){
-            current = current->lchild;
-        } else {
-            current=current->rchild;
-        }
-    }
-    return -1;
-}
-
-
-// ricerca ricorsiva di k
-
-bool Node:: searchR(int k){
-    if (this==NULL){
-        cout << "non trovato" << " ";
-        return false;
-    }
-    if (this->data==k){
-        cout << "trovato" << " ";
-        return true;
-    }
-    if (k<this->data){
-        return this->lchild->searchR(k);
-    } else {
-        return this->rchild->searchR(k);
-    }
-}
-
-// inserimento iterativo di k
-
-Node* Node::insertI(int k) {
-    Node *current{this};
-    Node *parent{nullptr};
-    while (current != NULL) {
-        if (current->data == k) {
-            current->weight++;
-            return this;
-        }
-        if (k < current->data) {
-            parent = current;
-            current = current->lchild;
-        } else {
-            parent = current;
-            current = current->rchild;
-        }
-    }
-
-    Node *n = new Node(k);
-    if (k < parent->data) {
-        parent->lchild = n;
-    } else {
-        parent->rchild = n;
-    }
-    return this;
-}
-
-
-
 #include <iostream>
-#include <cstdlib>
-#include <vector>
+#include "Node.h"
 using namespace std;
 
-
-class nodo{
-    private:
-    int data;
-    int weight;
-    nodo*right;
-    nodo*left;
-
-
-    public:
-
-    nodo(int v){
-        data=v;
-        weight=1;
-        right=nullptr;
-        left=nullptr;
+    Node* Node::getRchild(){
+        return rchild;
     }
 
-    nodo*Insert_Iterattivo (nodo*r , int k){ //INSERIMENTO ITERATTIVO
+    Node* Node::getLchild(){
+        return lchild;
+    }
 
-    nodo*current{r};
-    nodo*father{nullptr};
-    bool check=true;
+    void Node::setRchild(Node* Root){
+        rchild = Root;
+    }
 
-    while(check){
-        if(current==NULL){
-            if(k < father->data){
-                father->left = new nodo(k);
+    void Node::setLchild(Node* Root){
+        lchild = Root;
+    }
+
+    int Node::getValue(){
+        return value;
+    }
+
+
+    
+    Node* Node::Insert_It(int k){
+        Node* NewNode = new Node(k);    
+        Node* current{this};
+        Node* parent{nullptr};
+
+        //Caso Base
+        if (current==nullptr)
+        {
+            return NewNode;
+        }
+
+        //Spostamento sull'albero
+        while (current != nullptr)
+        {
+            if (k < current->value)
+            {
+                current = current->lchild;
             }else{
-                father->right = new nodo(k);
+                current = current->rchild;
             }
-            return r;
+            
         }
-        if(current->data == k){
-            current->weight ++;
-            return r;
-        }
-        if(k< current->data){
-            father=current;
-            current = current->left;
-        }else{
-            father=current;
-            current= current->right:
-        }
-}
 
-void sort( nodo* r, vector<int>& v){
-    if(r==NULL){
-        return;
+        //Inserimento del nuovo Nodo
+        if (k < parent->value)
+        {
+            parent->lchild = NewNode;
+        }else{
+            parent->rchild = NewNode;
+        }
     }
-    sort(r->left,v);
-    v.push_back(r->data);
-    sort(r->right,v);
-    return;
-}
+
+
+
+    // Inserimento Ricorsivo
+    Node* Node::Insert_Rs(int k){
+        Node* NewNode = new Node(k);
+        Node* current{this};
+        
+        //Caso Base
+
+        if (current==nullptr)
+        {
+            return NewNode;
+        }
+
+        //Se k è minore del nodo SINISTRA
+
+        if (k < current)
+        {
+            if (current->lchild == nullptr)
+            {
+                return NewNode;
+            }else{
+                current->lchild = current->lchild->Insert_Rs(k);
+            }
+            
+            
+        }
+        
+        //Se Destra
+
+        if (k > current)
+        {
+            if (current->rchild ==nullptr)
+            {
+                return NewNode;
+            }else{
+                current->rchild = current->rchild->Insert_Rs(k);
+            }
+
+            
+        }
+        
+        return current
+        
+
+    }
+
+
+
+    //Search Iterativo
+    Node* Node::Search_It(int k){
+        Node* current{this};
+
+        //Caso Albero Vuoto 
+        if (current == nullptr)
+        {
+            return nullptr;
+        }
+        
+        while (current->value!=k)
+        {
+            if (current->value==k)
+            {
+                return current;
+            }
+            if (k<current->value)
+            {
+                current= current->lchild;
+            }else{
+                current = current->rchild;
+            }
+            
+        }
+        return nullptr;
+    }
+
+
+
+
+    //Search Ricorsivo
+    Node* Node::Search_Rs(int k){
+        Node* current {this};
+        
+        //Caso base
+        if (current == nullptr)
+        {
+            return nullptr;
+        }
+        if (current->value == k)
+        {
+            return current;
+        }
+        
+
+        if (k < current->value)
+        {
+            return current->lchild->Search_Rs(k);
+        }else{
+            return current->rchild->Search_Rs(k);
+        }
+        
+        return nullptr;
+
+    }
+
+
+    void Node::preOrder(){
+        cout << this->value<<" ";
+        if (this->lchild!=nullptr)
+        {
+            this->lchild->inOrder();
+        }
+        if (this->rchild!=nullptr)
+        {
+            this->rchild->inOrder();
+        }
+        
+    }
+
+    void Node::postOrder(){
+        if (this->lchild!=nullptr)
+        {
+            this->lchild->inOrder();
+        }
+        if (this->rchild!=nullptr)
+        {
+            this->rchild->inOrder();
+        }
+        cout << this->value<<" ";
+    }
+
+    void Node::inOrder(){
+        if (this->lchild!=nullptr)
+        {
+            this->lchild->inOrder();
+        }
+        cout << this->value<<" ";
+        if (this->rchild!=nullptr)
+        {
+            this->rchild->inOrder();
+        }
+        
+    }
